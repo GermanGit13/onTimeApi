ad26f4eb47d9eebf16eb06cd884354b743f4ec91
GermanGit13 <99618445+GermanGit13@users.noreply.github.com>
2023-10-29 17:29:10 +0100

    Create Util Literal Create domain User

---
+++ src/main/java/com/svalero/onTimeApi/Util/Literal.java
@@ -0,0 +1,14 @@
+package com.svalero.onTimeApi.Util;
+
+public class Literal {
+
+    public static final String USER = "users";
+    public static final String SIGN = "signs";
+    public static final String DESK = "desks";
+    public static final String BOOKING ="bookings";
+
+    public static final String LITERAL_NOT_BLANK= "El campo no puede estar en blanco";
+    public static final String LITERAL_NOT_NULL= "El campo es obligatorio";
+    public static final String ROL_DEFAULT= "USER";
+
+}
