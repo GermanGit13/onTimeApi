6acf8e905da3319a28d8861303a6a82a1faed8ad
GermanGit13 <99618445+GermanGit13@users.noreply.github.com>
2023-10-29 18:36:22 +0100

    Create domain Booking

--- src/main/java/com/svalero/onTimeApi/domain/Booking.java
+++ src/main/java/com/svalero/onTimeApi/domain/Booking.java
@@ -1,4 +1,41 @@
 package com.svalero.onTimeApi.domain;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Time;
+import java.util.Date;
+
+import static com.svalero.onTimeApi.Util.Literal.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = BOOKING)
 public class Booking {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column
+    @NotNull(message = LITERAL_NOT_NULL)
+    @NotBlank(message = LITERAL_NOT_BLANK)
+    private Date day;
+
+    @Column
+    @NotNull(message = LITERAL_NOT_NULL)
+    @NotBlank(message = LITERAL_NOT_BLANK)
+    private Time inBooking;
+
+    @Column
+    @NotNull(message = LITERAL_NOT_NULL)
+    @NotBlank(message = LITERAL_NOT_BLANK)
+    private Time outBooking;
+
+
 }
