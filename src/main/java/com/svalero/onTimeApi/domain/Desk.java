b8baf8c16f818ba3d3c61206fee4d620944c6131
GermanGit13 <99618445+GermanGit13@users.noreply.github.com>
2023-10-29 18:26:26 +0100

    Create domain Desk

--- src/main/java/com/svalero/onTimeApi/domain/Desk.java
+++ src/main/java/com/svalero/onTimeApi/domain/Desk.java
@@ -1,4 +1,31 @@
 package com.svalero.onTimeApi.domain;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import static com.svalero.onTimeApi.Util.Literal.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = DESK)
 public class Desk {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column
+    @NotBlank(message = LITERAL_NOT_BLANK)
+    @NotNull(message = LITERAL_NOT_NULL)
+    private int floor;
+
+    private boolean desk;
+
+    private boolean meetingRoom;
+
 }
