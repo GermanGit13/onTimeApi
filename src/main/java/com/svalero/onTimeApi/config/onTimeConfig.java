61eb137fd60b3935ab75aa09c53ed9a29c1052cc
GermanGit13 <99618445+GermanGit13@users.noreply.github.com>
2023-11-01 18:34:47 +0100

    Created Config access from all API ModelMapper

---
+++ src/main/java/com/svalero/onTimeApi/config/onTimeConfig.java
@@ -0,0 +1,20 @@
+package com.svalero.onTimeApi.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class onTimeConfig {
+
+    /**
+     * @Configuration: Clase de configuración para que tenga acceso desde cuaqluier parte del proyecto.
+     * @Bean: Se usa junto con @Configuration -> para los métodos que creamos y queremos usar en el proyecto.
+     * ModelMapper: Libreria para mapear campos entre objetos.
+     */
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+}
